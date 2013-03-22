23dc3809e9dd90b70a0ebf4274a15f6025923e9e
cfly <caofei.org@gmail.com>
2013-03-22 18:13:27 +0800

    js

--- src/main/java/org/caofei/bean/Person.java
+++ src/main/java/org/caofei/bean/Person.java
@@ -1,9 +1,19 @@
 package org.caofei.bean;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.collections.functors.InstantiateFactory;
+import org.apache.commons.collections.list.LazyList;
+
 public class Person {
 	private String id;
 	private String name;
 	private String age;
+	
+	private List<PersonDetail> personList = LazyList.decorate(new LinkedList<PersonDetail>() ,new InstantiateFactory(PersonDetail.class));
+	
+	
 	public String getId() {
 		return id;
 	}
@@ -21,5 +31,11 @@ public class Person {
 	}
 	public void setAge(String age) {
 		this.age = age;
+	}
+	public List<PersonDetail> getPersonList() {
+		return personList;
+	}
+	public void setPersonList(List<PersonDetail> personList) {
+		this.personList = personList;
 	}
 }
